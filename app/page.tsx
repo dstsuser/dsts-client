3e9a31dcb8a097ad9574e6bd2f475daba14a501f
dstsuser <164944611+dstsuser@users.noreply.github.com>
2024-04-08 00:44:30 +0600

    photo frame added

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ import HomeCarousel from '@/components/Carousel/HomeCarousel';
 import { Container, Grid } from '@mantine/core';
 import { BriefAndNotice } from '@/components/BriefAndNotice/BriefAndNotice';
 import { HeroImage } from '@/components/HeroImage/HeroImage';
-import { PhotoGrid } from '@/components/PhotoGrid/PhotoGrid';
 import { GetInTouch } from '@/components/Contact/GetInTouch';
 import AboutAndGoal from '@/components/AboutAndGoal/AboutAndGoal';
 import AboutUs from '@/components/AboutAndGoal/About';
 import Goal from '@/components/AboutAndGoal/Goal';
 import { ContactUs } from '@/components/ContactUs/ContactUs';
+import PhotoGrid from '@/components/PhotoGrid/PhotoGrid';
 
 
 export default function HomePage() {
--- components/PhotoGrid/PhotoGrid.tsx
+++ components/PhotoGrid/PhotoGrid.tsx
@@ -1,70 +1,29 @@
-import { useGetPhotoFramesQuery } from '@/lib/redux/features/photoFrames/photoFramesApi';
-import { SimpleGrid, Skeleton, Container, Stack, useMantineTheme, px, Image, Grid, GridCol } from '@mantine/core';
+'use client'
+import { useGetPhotoFramesQuery } from '@/lib/redux/features/photoFrames/photoFramesApi'
+import { Container, Grid, Image } from '@mantine/core'
+import React from 'react'
+const classes = require('./PhotoGrid.module.css')
 
-const getChild = (height: number) => <Skeleton height={height} radius="md" animate={false} />;
-const BASE_HEIGHT = 360;
-const getSubHeight = (children: number, spacing: number) =>
-  BASE_HEIGHT / children - spacing * ((children - 1) / children);
-
-export function PhotoGrid() {
+export default function PhotoGrid() {
 
   const {data,isLoading,isError} = useGetPhotoFramesQuery('')
 
-  const photo1 = <img src="https://source.unsplash.com/random/800x800" alt="Photo 1" />;
-  const photo2 = <img src="https://source.unsplash.com/random/800x800" alt="Photo 2" />;
-  const photo3 = <img src="https://source.unsplash.com/random/800x800" alt="Photo 3" />;
-const photo4 = <img src="https://source.unsplash.com/random/800x800" alt="Photo 4"  />
-
-  let photoFrameContent = <div></div>
-  if(isLoading){
-    photoFrameContent = <div>Loading...</div>
-  }else if(isError){
-    photoFrameContent = <div>Error...</div>
-  }else if(data?.photoFrames.length<=0){
-    photoFrameContent = <div>No data...</div>
-  }else if(data?.photoFrames.length>0){
-    photoFrameContent = 
-    <Grid>
-        <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
-            <Grid>
-              <GridCol span={{base:12, md:6,lg:6}}>
-                <div style={{height:'300px',overflow:'hidden'}}>
-                  <Image height={'100%'} src={data.photoFrames[0].imageLink} alt="Photo 1" />
-                </div>
-              </GridCol>
-              <GridCol span={{base:12, md:6,lg:6}}>
-                <div style={{height:'150px'}}>
-                  <Image height={'100%'} src={data.photoFrames[0].imageLink} alt="Photo 2" />
-                </div>
-                <div style={{height:'150px'}} >
-                  <Image  height={'100%'} src={data.photoFrames[0].imageLink} alt="Photo 2" />
-                </div>
-              </GridCol>
-            </Grid>
-        </Grid.Col>
-
-        <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
-            <Grid>
-              <Grid.Col span={{base:12, md:6,lg:6}}>
-                <div style={{height:'150px'}}>
-                  <Image height={'100%'} src={data.photoFrames[0].imageLink} alt="Photo 1" />
-                </div>
-                <div style={{height:'150px'}}>
-                  <Image height={'100%'} src={data.photoFrames[0].imageLink} alt="Photo 1" />
-                </div>
-              </Grid.Col>
-            </Grid>
-        </Grid.Col>
-    </Grid>
-  }
-
-
-  const theme = useMantineTheme();
-
 
   return (
-    <Container size={'lg'} mah={'300px'}>
-        {photoFrameContent}
-    </Container>
-  );
+    <div>
+        <Container size={'lg'} className={classes.wrapper}>
+            <Grid>
+              {isLoading ? <div>Loading...</div> : isError ? <div>Error...</div> : data?.photoFrames?.slice(0,6)?.map((item:any) => {
+                return(
+                  <Grid.Col span={{base:12,md:4,lg:4}}>
+                    <div className={classes.gridItem}>
+                        <Image className={classes.img} src={item?.imageLink} alt={item.altText} width={250} height={250} />
+                    </div>
+                  </Grid.Col>  
+                )
+              } )}
+            </Grid>
+        </Container>
+    </div>
+  )
 }
